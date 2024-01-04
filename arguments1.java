26a690b589cba9be9a61c8a9955de06c6d2a4c6d
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-04 16:29:53 +0530

    Add files via upload

---
+++ arguments1.java
@@ -0,0 +1,30 @@
+class student 
+{
+	int admno;
+	String name;
+	student (int x, inty)
+	{
+		admno=x;
+		name=y;
+	}
+	void show()
+	{
+		System.out.println("Admno="+admno+"Name="+name);
+	}
+
+}
+public Static void main (String[] args)
+{
+	student s1=new student(222,"rrr");
+	s1.show();
+}
+}
+
+
+
+
+
+
+	
+	
+	
