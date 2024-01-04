10ef5c106b945f789e5fa9b2cfa0324a49f81406
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-04 16:28:20 +0530

    Add files via upload

---
+++ constructor1.java
@@ -0,0 +1,26 @@
+class sample
+{
+	void m1()
+	{
+		System.out.println("m1 method");
+	}
+	sample ()
+	{
+  System.out.println("Constructor");
+	}
+	public static void main(String[] args)
+	{
+		sample s1=new sample();
+        sample s2=new sample();
+		s1.m1();
+	}
+}
+
+
+
+
+
+
+	
+	
+	
