a89aa16da564e9c6478d3792fc6f1f90120f58c3
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-04 09:46:05 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Loop
+{
+    Scanner s=new Scanner(System.in);
+public static void main (String []args)
+{
+
+    int i;
+    for (i=1;i<=10;i++)
+    {
+		System.out.print(i+ "");
+	}
+}
+}  
