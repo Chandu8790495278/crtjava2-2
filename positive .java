32f0884628094f7b696f9064e73adead1f18c8a1
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-04 10:04:52 +0530

    Add files via upload

---
+++ positive .java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Positive 
+{
+ public static void main(String[] args)
+	{
+
+     Scanner s=new Scanner(System.in);
+	 int a;
+	System.out.println(" Enter a number number");
+	a=s.nextInt();
+	
+	if ((a>0)
+		{
+         System.out.println(" positive number");
+		}
+		else(a<0)
+		
+			System.out.println("Negative number");
+		}
+	
+	else
+	{
+		System.out.println("zero");
+	}
+}
+	
+
+	
+		
+
+	
+
+	
+			
