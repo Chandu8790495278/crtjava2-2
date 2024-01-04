b4e609335ebc7e2d5430013bb07fc80351c65a5b
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-04 10:06:38 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Vowel
+{
+ public static void main(String[] args)
+	{
+
+     Scanner s=new Scanner(System.in);
+	char a;
+
+
+		 System.out.println("Enter a string"); 
+		 
+      a=s.nextInt().char At(0);
+	  if(a=='a'|| a=='e'||a=='i'||a=='o'|| a=='u'||
+		  a=='A'|| a=='E'|| a=='I'|| a=='o'|| a=='U'||)
+		{
+
+	  if(n%2==0)
+		{
+		  System.out.println("vowel");
+		}
+		else
+			{
+
+		  
+
+			else{ System.out.println("consonant");
+			}
+			}
+		}
+
+			 
+		
+
+
+
+      
+
+
+	
+	
+		
+	
