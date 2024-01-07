4baa6a618d909769e8dc038cc1ce0ee97dba6012
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 15:46:59 +0530

    Add files via upload

---
+++ Hierarchical inheritence.java
@@ -0,0 +1,55 @@
+	
+	class Data
+{
+int a,bl;
+void get(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	void show()
+	{
+		System.out.println("a="+a+"b="+b);
+	}
+}
+		System.out.println("Name="+name);
+	}
+}
+class Addition extends Data
+	{
+	void total()
+		{
+		System.out.println("Addition="+(a+b));
+		}
+	}
+	class multiply extands Data{
+
+	{
+		void product ()
+		{
+          System.out.println("product="+(a*b));
+		}
+	}
+	class Single
+	{
+	public static void main(String args[])
+			{
+		Addition a1=new Addition();
+				a1.get(100,223);
+				a1.show();
+				r1.total();
+				multiply m1=new multiplt();
+				m1.get(22,75);
+				r1.show ();
+				m1.product();
+			}
+		}
+	
+
+					
+									
+				
+
+		 
+		       
+	
