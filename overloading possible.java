32346e7d7dc6e59943c2f1dbd5d936a884d2fbff
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 15:55:13 +0530

    Add files via upload

---
+++ overloading possible.java
@@ -0,0 +1,19 @@
+class first
+{
+	void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	void add(int a,int b,int c)
+	{
+		System.out.println(a+b+c)
+	}
+}
+class Second extends First
+{
+	void add (int a.int b.int c.int d)
+	{
+		System.out.println(a+b+c+d);
+	}
+}
+
