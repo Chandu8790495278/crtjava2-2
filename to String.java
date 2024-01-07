8ef29430efaaf185fa6d1d7c56cdc34d312d8ba7
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 16:26:04 +0530

    Add files via upload

---
+++ to String.java
@@ -0,0 +1,24 @@
+class student
+
+{
+	student (int admno,String name)
+	{
+		this.admno=admno;
+		this.name=name;
+	}
+	public String to String()
+	{
+		return "admission no=admno+"Name="+name;
+	}
+}
+class Test
+{
+	public static void main(String[] args)
+	{
+		Student s1=new Student(111,"RRR");
+		Student s2=new Student(444,"KKk");
+		System.out.println(s1.to String());
+		System.out.println(s2);
+	}
+}
+	
