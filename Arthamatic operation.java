324fc9d6e8c5170d23f0c8d34639f789ef20962a
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 15:45:56 +0530

    Add files via upload

---
+++ Arthamatic operation.java
@@ -0,0 +1,20 @@
+Package chinni;
+public class Arth
+{
+	public int add (int a,int b)
+	{
+		return a+b;
+	}
+	public int sub(int a,int b)
+	{
+		reurn a-b;
+	}
+	public int div(int a,int b)
+	{
+		return a/b;
+	}
+}
+
+
+
+
