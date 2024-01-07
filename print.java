f8819f374ba21a014fbc43fbd42484cba102d3d7
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 16:22:16 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,32 @@
+class  
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a[],i;
+		a=new int[5];
+	     System.out.print( " Enter size of the array");
+		n=s.nextInt();
+		a=new int[n];
+		System.out.println("Enter n elements");
+		for(i=0;i<n;i++);
+		{
+			a[i]=s.nextInt();
+		}
+		System.out.printl("The values are");
+		for(i=0;i<n;i++);
+		{
+
+		{
+			a[i]=s.nextInt();
+		}
+		System.out.println("The values are");
+		for(i=0;i<5;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
+		
+			
+	
