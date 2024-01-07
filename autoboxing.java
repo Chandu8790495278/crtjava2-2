dfd530c29115cd1c1ca48c91058b16d4e2ad29c3
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 15:46:33 +0530

    Add files via upload

---
+++ autoboxing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Test
+{
+	public static void main(String[] args)
+	{
+		int a=34;
+		Integer b=a;
+		System.out.println(a);
+		System.out.println(b);
+		Double c=56.7;
+		double d=c;
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
+
+
