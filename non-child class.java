6509b2a68d931cb06fd2c55307a10382b2b37b49
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-07 15:57:17 +0530

    Add files via upload

---
+++ non-child class.java
@@ -0,0 +1,20 @@
+Package pack2;
+import pack1.First;
+class Fifth
+{
+	void show5()
+	{
+		First f1=new First();
+		// System.out.println("private a="+f1.a);
+        // System.out.println("default b="+f1.b);
+        // System.out.println("protected c="+f1.c);
+		System.out.println("public="+f1.d);
+	}
+	public static void main(String[] args)
+	{
+		Fifth f1=new Fifth();
+		f2.show5();
+	}
+}
+
+
