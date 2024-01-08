b3156e9649e986b1ff0e0b994224962076405032
Chandu8790495278 <155130498+Chandu8790495278@users.noreply.github.com>
2024-01-08 13:27:17 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,27 @@
+class first
+{
+	void main
+	{
+		System.out.println("m1 method in first class");
+	}
+	void m2()
+	{
+		system.out.println("m2 method in first class");
+	}
+}
+class second extends first
+{
+	void m3()
+	{
+		System.out.println("m3 method in second class");
+	}
+}
+class Test
+{
+	public Ststic void main (String args[])
+	{
+		second s1=new send();
+		s1.m1;
+		s1.m2;
+	}
+}
